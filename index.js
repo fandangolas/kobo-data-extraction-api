4b22e85591c02c0d6aaba133af0004c1102a429d
Nicolas <nicolas.knoxx@gmail.com>
2020-05-01 18:17:56 -0300

    adicionando conexão com mongoDB e adicionando container do mongo no docker-compose

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const PORT = 3000
 const HOST = '0.0.0.0'
 
 const app = express()
+
+mongoose
+  .connect('mongodb://db:27017/crud-node-mongo-docker', {
+    useNewUrlParser: true
+  })
+  .then(result => {
+    console.log('MongoDB Conectado');
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')

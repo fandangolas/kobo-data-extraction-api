6823df401e61bf572cc596ba7f703afd293393b2
Nícolas B <fandangolas@localhost.localdomain>
2020-05-25 01:32:14 -0300

    método de conexão com mongodb e renomeando função

--- src/app/database.js
+++ src/app/database.js
@@ -1,19 +1,37 @@
 import { MongoClient } from "mongodb";
 
-const mongoDbConnect = callback => {
-  const uri = process.env.MONGO_DB_CONNECTIONSTRING;
-  const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const mongoDb = () => {
+  let _db;
+
+  const connect = callback => {
+    const uri = process.env.MONGO_DB_CONNECTIONSTRING;
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    };
+  
+    MongoClient.connect(uri,options)
+      .then(client => {
+        _db = client.db();
+        callback(client);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
-  MongoClient.connect(uri,options)
-    .then(client => {
-      callback(client);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
+  const getDb = () => {
+    if (_db) {
+      return _db;
+    }
 
-module.exports = mongoDbConnect;
+    throw 'No database found!';
+  };
+
+  return {
+    connect,
+    getDb
+  };
+};
+
+module.exports = mongoDb;

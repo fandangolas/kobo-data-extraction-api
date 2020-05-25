b851836c03c56cea490dd002f123a526211d2a61
Nícolas B <fandangolas@localhost.localdomain>
2020-05-25 01:32:32 -0300

    adaptando index.js as renomeações

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import express from "express";
 import router from "./routes/auth";
 import configureContainer from "./app/configureContainer";
-import mongoDbConnect from "./app/database";
+import mongoDb from "./app/database";
 
 dotenv.config();
-
 const container = configureContainer();
+const mongoDbClient = mongoDb();
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 app.use(router);
 
-mongoDbConnect(client => {
+mongoDbClient.connect(client => {
   console.log(`Connected to Db: ${client.s.options.dbName}`);
 
   app.listen(process.env.PORT, process.env.HOST);

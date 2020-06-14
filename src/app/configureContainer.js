7bf65fbedbba978b90334f9daa635bb1a071e669
Nícolas B <fandangolas@localhost.localdomain>
2020-06-14 03:58:39 -0300

    adicionando middleware de autorização no kobo

--- src/app/configureContainer.js
+++ src/app/configureContainer.js
@@ -1,4 +1,5 @@
 import authController from "../controllers/authController";
+import authMiddleware from "../middlewares/authMiddleware";
 import authRouter from "../routes/auth";
 import { createContainer, asValue, asFunction } from "awilix";
 import koboClient from "../infrastructure/koboClient";
@@ -20,6 +21,10 @@ const configureContainer = () => {
     //Register Kobo-API client 
     .register({
       koboClient: asFunction(koboClient).singleton()
+    })
+    //Register Middlewares
+    .register({
+      authMiddleware: asFunction(authMiddleware)
     })
     //Register Controllers
     .register({

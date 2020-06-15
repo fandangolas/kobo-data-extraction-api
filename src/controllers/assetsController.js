41d8f1984953ad32d297d7940a0a4b58c75d2c7f
Nícolas B <fandangolas@localhost.localdomain>
2020-06-15 01:10:04 -0300

    removendo testes da controller

--- src/controllers/assetsController.js
+++ src/controllers/assetsController.js
@@ -1,4 +1,4 @@
-const assetsController = ({ db, koboClient, assetsRepository, assetsDataRepository }) => {
+const assetsController = ({ koboClient, assetsRepository, assetsDataRepository }) => {
 
   const getAssets = async (req, res) => {
     const data = await koboClient.getAssets(req.koboToken);
@@ -8,29 +8,16 @@ const assetsController = ({ db, koboClient, assetsRepository, assetsDataReposito
 
   const registerAsset = async (req, res) => {
     const uid = req.params.uid;
-    const connection = db.getDb();
 
     const asset = await koboClient.getAsset(req.koboToken, uid);
     const assetData = await koboClient.getData(req.koboToken, uid);
 
-    const { insertedId: assetInsertionId } = await assetsRepository.createAsset({ asset: "assets repository test 6"});
+    //const { insertedId: assetInsertionId } = await assetsRepository.createAsset({ asset: "assets repository test 6"});
     const { insertedId: assetDataInsertionId } = await assetsDataRepository.createAssetData({ data: assetData });
 
-    const assets = await connection.collection('assets')
-      .find({ })
-      .toArray();
-
-    const assetsData = await connection.collection('assets-data')
-      .find({ })
-      .toArray();
-
-    console.log(assets, assetsData);
-
     res.status(202).json({
-      assetInsertionId,
-      assetDataInsertionId,
-      //asset,
-      assetData
+      //assetInsertionId,
+      assetDataInsertionId
     });
   };
 

4ec97e5f5ee8145ef6b018aac26dc965eea71d2d
Nícolas B <fandangolas@localhost.localdomain>
2020-06-15 00:06:35 -0300

    voltando implementação da controller

--- src/controllers/assetsController.js
+++ src/controllers/assetsController.js
@@ -1,24 +1,8 @@
-const assetsController = ({ koboClient, db }) => {
-  const getAssets = async (req, res, next) => {
-    const connection = db.getDb();
-    const surveys = await koboClient.getAssets(req.koboToken);
+const assetsController = ({ koboClient }) => {
+  const getAssets = async (req, res) => {
+    const data = await koboClient.getAssets(req.koboToken);
 
-    const firstSurvey = surveys.results[0];
-    const firstUid = surveys.results[0].uid;
-    
-    const firstSurveyData = await koboClient.getData(firstUid, req.koboToken);
-
-    const dbRegisters = await connection.collection('test')
-      .find({ })
-      .toArray();
-
-    console.log(dbRegisters);
-
-    res.json({
-      surveys,
-      firstSurvey,
-      firstSurveyData
-    });
+    res.json({ data });
   };
 
   return { getAssets };
